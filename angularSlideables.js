01709a3672eaf53dad8493791eb6d8b757cb65d7
Poul Handleman <pavel.handleman@gmail.com>
2015-06-03 13:14:40 +0300

    more convenient approach with use of transclude option

--- angularSlideables.js
+++ angularSlideables.js
@@ -2,24 +2,19 @@ angular.module('angularSlideables', [])
 .directive('slideable', function () {
     return {
         restrict:'C',
-        compile: function (element, attr) {
-            // wrap tag
-            var contents = element.html();
-            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
-
-            return function postLink(scope, element, attrs) {
-                // default properties
-                attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
-                attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
-                element.css({
-                    'overflow': 'hidden',
-                    'height': '0px',
-                    'transitionProperty': 'height',
-                    'transitionDuration': attrs.duration,
-                    'transitionTimingFunction': attrs.easing
-                });
-            };
-        }
+		transclude: true,
+		template:'<div class="slideable_content" style="margin:0 !important; padding:0 !important" ng-transclude></div>',
+		link: function (scope, element, attrs) {
+			attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
+			attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+			element.css({
+				'overflow': 'hidden',
+				'height': '0px',
+				'transitionProperty': 'height',
+				'transitionDuration': attrs.duration,
+				'transitionTimingFunction': attrs.easing
+			});
+		}
     };
 })
 .directive('slideToggle', function() {
